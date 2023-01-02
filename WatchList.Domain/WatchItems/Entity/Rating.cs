3a70e5a9d3af7491887fa0dafa99955ddaa70f30
Dmitry Sushchevsky <d.suschevsky.personal@gmail.com>
2023-01-02 15:27:41 +0300

    Blazor UI - Fixed filter by Rating

--- WatchList.Domain/WatchItems/Entity/Rating.cs
+++ WatchList.Domain/WatchItems/Entity/Rating.cs
@@ -1,17 +1,20 @@
 ﻿namespace WatchList.Domain.WatchItems.Entity;
 
-public readonly struct Rating : IEquatable<Rating>
+public readonly struct Rating : IEquatable<Rating>, IComparable<Rating>, IComparable
 {
     private readonly float _value;
 
     public Rating(float value) => _value = value;
 
+    public static bool operator <(Rating lft, Rating rgt) => lft.CompareTo(rgt) < 0;
+    public static bool operator >(Rating lft, Rating rgt) => lft.CompareTo(rgt) > 0;
+    public static bool operator !=(Rating lft, Rating rgt) => !(lft == rgt);
+    public static bool operator ==(Rating lft, Rating rgt) => lft.Equals(rgt);
     public static implicit operator float(Rating rating) => rating._value;
     public bool Equals(Rating other) => _value.Equals(other._value);
-
+    public int CompareTo(Rating other) => _value.CompareTo(other._value);
+    public int CompareTo(object? obj) => obj is Rating r ? CompareTo(r) : 1;
     public override bool Equals(object? obj) => obj is Rating other && Equals(other);
-
     public override int GetHashCode() => _value.GetHashCode();
-
     public override string ToString() => _value.ToString("0.0");
 }
