b24a3a8fa6104e04309129e47aa95a8879bae155
Dmitry Sushchevsky <d.suschevsky.personal@gmail.com>
2023-01-02 14:53:07 +0300

    BriefWatchItemDetailFilter - Added ability filter by ReleaseDate

--- WatchList.Domain/WatchItems/Repository/BriefWatchItemDetailFilter.cs
+++ WatchList.Domain/WatchItems/Repository/BriefWatchItemDetailFilter.cs
@@ -8,16 +8,33 @@ public sealed class BriefWatchItemDetailFilter
     public string? Title { get; set; }
     public Status Status { get; set; } = Status.Unknown;
     public WatchItemType Type { get; set; } = WatchItemType.Unknown;
+    public DateTime? ReleaseDateFrom { get; set; }
+    public DateTime? ReleaseDateTo { get; set; }
 
     public IQueryable<WatchItem> ApplyFilter(
         IQueryable<WatchItem> queryable)
     {
         if (!string.IsNullOrEmpty(Title))
             queryable = queryable.Where(e => e.Title.Contains(Title));
+        
         if (Status != Status.Unknown)
             queryable = queryable.Where(e => e.Status == Status);
+        
         if (Type != WatchItemType.Unknown)
             queryable = queryable.Where(e => e.Type == Type);
+
+        if (ReleaseDateFrom != null)
+        {
+            var releaseDateFrom = DateOnly.FromDateTime(ReleaseDateFrom.Value);
+            queryable = queryable.Where(e => e.ReleaseDate >= releaseDateFrom);
+        }
+        
+        if (ReleaseDateTo != null)
+        {
+            var releaseDateTo = DateOnly.FromDateTime(ReleaseDateTo.Value);
+            queryable = queryable.Where(e => e.ReleaseDate <= releaseDateTo);
+        }
+        
         return queryable;
     }
 }
